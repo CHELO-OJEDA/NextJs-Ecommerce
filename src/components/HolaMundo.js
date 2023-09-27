209d556e5a5ad3fdd3a5d4b8621a50795f5e0e57
CHELO-OJEDA <cheloojeda9@gmail.com>
2023-09-26 21:39:07 -0400

    Proyecto

---
+++ src/components/HolaMundo.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function HolaMundo() {
+    const mifinction = () => {
+      
+    }
+    return (    
+    <>
+      <h1>Hola Mundo</h1>
+      <h2>Agustin Navarro</h2>
+    </>
+  )
+}
+
