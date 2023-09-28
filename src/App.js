605dd5dd6e65a592058552b6351f253e7a1ea754
CHELO-OJEDA <cheloojeda9@gmail.com>
2023-09-27 21:49:50 -0400

    Saludar props

--- src/App.js
+++ src/App.js
@@ -2,14 +2,15 @@ import logo from './logo.svg';
 import './App.css';
 import HolaMundo from './components/HolaMundo';
 import AdiosMundo from './components/AdiosMundo';
+import Saludar from './components/Saludar';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <HolaMundo/>
-        <AdiosMundo/>
+        <Saludar name="Agustin Navarro Galdon" edad="26"/>
+        <Saludar name="Paco" edad="31"/>
       </header>
     </div>
   );
